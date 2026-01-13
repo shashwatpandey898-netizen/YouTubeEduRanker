07e0043793f8b5c998dbc034ddd72f2ad8e74f52
shashwatpandey898-netizen <shashwatpandey898@gmail.com>
2026-01-13 23:17:14 +0530

    for hosting 2

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-for-eduranker'
     # API Key provided in project request
-    GEMINI_API_KEY = "AIzaSyDTrUUAk-mVC9DAcvhuSGb1_efgyPwoaiY"
+    GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
     DEBUG = True
